d3be2c7d5c09d83c8938857c1ddcaa9d5aaf69da
DiegoBear <47014059+DiegoBear@users.noreply.github.com>
2022-02-02 15:40:51 -0400

    Update to config for Wolastoqey

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
-  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  language: 'Wolastoqey', // This changes the display name for your language
+  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  author: 'Diego Bear', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'https://www.maliseet.org/, // Put a link to your website or social media here
+  wordListSource: 'Passamaquoddy-Maliseet Language Portal', // Describe the source material for your words here
+  wordListSourceLink: 'https://pmportal.org/', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
@@ -13,3 +13,4 @@ export const CONFIG = {
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
 }
+
